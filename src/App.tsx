7f223d0c3aecc515994ee1d09e44afa6ef455115
TormodNy <tormodn99@gmail.com>
2024-09-10 23:00:43 +0200

    set and get state from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { BoxContainer } from "./components/BoxContainer";
 import { AddBoxButton } from "./components/AddBoxButton";
 import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
+import { createTheme, IconButton } from "@mui/material";
 import { BoxType, IBox } from "./types";
+import { Save } from "@mui/icons-material";
 
 const darkTheme = createTheme({
   palette: {
@@ -24,12 +25,27 @@ export const AppContext = createContext<IAppContext>({
 function App() {
   const [boxes, setBoxes] = useState<IBox[]>([{ boxType: BoxType.Undefined }]);
 
+  useEffect(() => {
+    const localStorageBoxes = localStorage.getItem("state");
+    if (localStorageBoxes) {
+      setBoxes(JSON.parse(localStorageBoxes));
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <AppContext.Provider value={{ boxes, setBoxes }}>
         <div className="w-full flex flex-col items-center p-10 gap-8">
           <BoxContainer />
           <AddBoxButton />
+
+          <IconButton
+            color="info"
+            sx={{ position: "fixed", top: 8, right: 8, zIndex: 1000 }}
+            onClick={() => localStorage.setItem("state", JSON.stringify(boxes))}
+          >
+            <Save />
+          </IconButton>
         </div>
       </AppContext.Provider>
     </ThemeProvider>
