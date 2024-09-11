ffc18ae8569372db954ea809adbb751e7aa5603a
TormodNy <tormodn99@gmail.com>
2024-09-11 16:19:18 +0200

    transport box only shows metro that leaves after 5 minutes

--- src/components/TransportBox.tsx
+++ src/components/TransportBox.tsx
@@ -8,7 +8,7 @@ export function TransportBox({ index }: TransportBoxProps) {
   return (
     <Box index={index} removable>
       <iframe
-        src="https://mon.ruter.no/departures/59.929213-10.71634/N4Igrgzgpgwg9gGzAWwHYBkCGBPOYAuIAXAGaYLQA0IARnJgE4AmExA2qBPnAA4AKCTAGMoASRbsQAOQDKAJSIzu-QSKIBWABwBmTQEYQAXWpMog7FCZLG+ACoBLZFGIAGagAt7TUxnuoorETAAL7UqCg0UAwA8iQAIlA8NmAMAcSa1Pj2+AjORCAAspgAVnAMXGBQqCAm9hCYNLlMtgyYqBA8ZfgFcKaBbMYgVQ1NxPgMlR5eUC3CANbwSGhjE1ChnMoCwmISRGzS8oqbqlAaOuoALEYmZjiW1gx2jnluIJ7eVeh+aUGhIOHISIxeKJZKpQIZEBZHJ5EAAMQYcAA5v4GAACLiYJj2ODVWr1RqWWbtTqPHp9diDYaEpjEMgUKBTUyzIQLRAoaqkcjQYKDABuUQgOM5ACZgkA"
+        src="https://mon.ruter.no/departures/59.929213-10.71634/N4Igrgzgpgwg9gGzAWwHYBkCGBPOYAuIAXAGaYLQA0IARnJgE4AmExA2qBPnAA4AKCTAGMoASRbsQAOQDKAJSIzu-QSKIBWABwBmdQBYQ1WQqW8BwqBp2aAjCAC61JlEHYoTJY3wAVAJbIoYnVqAAtfJmcMX1QoViJgAF9qVBQaKAYAeRIAESgeLzAGWOJNanxffARAohAAWUwAKzgGLjAoVEMQJl8ITBoqpm8GTFQIHmb8WrhnOLYQfGHkTppIB2p2voHiBbbQ8Kgh4QBreCQ0bYY2pM5lcxFxWel5RVvVSy1dA0culxx3TwYPn81QADHsIu10NFivEkiAUsg0pkcnkCkU4qV5hUqsQQAAxBhwADmMQYAAIuJhunAOk4ept3IdRuNAVMZuxvht+u5iGQKFBwQdhkITogUB1SORoAlvgA3dIQXw04gAJgSQA"
         className="w-full h-full rounded-sm scale"
       />
     </Box>
